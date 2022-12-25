05b444b5e51334d32bec3f43a45997c4757e7f10
Mariusz Perzynski <m.perzyk@interia.pl>
2022-12-25 13:56:25 +0100

    connect with database

--- ex1.py
+++ ex1.py
@@ -1,1 +1,16 @@
 import sqlite3
+
+database = sqlite3.connect("firstbase.db")  # connect / create base -> first base.db
+cursor = database.cursor() # navigating the database
+
+cursor.execute('''  
+               
+  CREATE TABLE scores(           
+     id integer,
+     name string,           
+     surname string)    
+           
+ ''')  # select questions to database , # scores - table
+
+database.commit()  # save changes to database
+database.close()   # close database
